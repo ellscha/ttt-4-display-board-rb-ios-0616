e55788047762fac23a2a48eaca4b77d528f242f1
Elli Scharlin <escharlin@wesleyan.edu>
2017-12-20 23:07:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,5 @@
 # out the current state.
 def display_board(board)
   board = [" #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------", " #{board[6]} | #{board[7]} | #{board[8]} "]
+  puts board
 end
